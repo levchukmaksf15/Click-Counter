620e17f9c1aa892dfe0684560dfbd47dfa0826e4
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-17 22:12:58 +0200

    If you want to count something you can click on the button_counter and do it

--- app/src/main/java/com/non_zero/click_counter/MainActivity.kt
+++ app/src/main/java/com/non_zero/click_counter/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.non_zero.click_counter
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            textView.text = "Hello Kitty"
+            it.setBackgroundColor(Color.GREEN)
+        }
+
+        button_counter.setOnClickListener{
+            textView.text = "I have counted ${++counter} crows"
+        }
     }
 }
