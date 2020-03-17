0c87f4058a6ad0623a2ec36a30b6849f61c1a19d
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-17 19:55:56 +0200

    First commit

---
+++ app/src/main/java/com/non_zero/click_counter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.non_zero.click_counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
